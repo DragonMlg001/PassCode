012de206ae4cc1ededc8111e7775b1f826a72b96
DragonMlg001 <albertrio47@gmail.com>
2024-05-06 18:04:55 +0500

    Added tailwind

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+<>
+  <nav className='bg-blue-600 text-white'
+  >
+  <ul>
+<li>
+    <a href='#'>Home</a>
+    <a href='#'>About</a>
+    <a href='#'>FeedBack</a>
+</li>
+  </ul>
+  </nav> 
+</>
+  )
+}
+
+export default Navbar
